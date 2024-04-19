0836e6ed55dc9e6cec4392988ae9bd4d0bd73701
korma <korma@JAKUBPC>
2024-04-19 19:46:35 +0200

    FE: ArticleBroserPage and AritclePage commit

--- FE/Noviny/src/App.jsx
+++ FE/Noviny/src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar';
 import AboutPage from './pages/AboutPage';
 import ArticleBrowserPage from './pages/ArticleBrowserPage';
 import AdminPage from './pages/AdminPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   const [pagePositon, setPagePosition] = useState(0);
@@ -33,9 +34,10 @@ function App() {
 
   const pages = [
     <HomePage key={0} onSelect={handleSelect} />,
-    <AboutPage key={1} onSelect={handleSelect} />,
+    <AboutPage key={1}/>,
     <ArticleBrowserPage key={2} onSelect={handleSelect} />,
-    <AdminPage key={3} onSelect={handleSelect} />,
+    <ArticlePage key={3}/>,
+    <AdminPage key={4} onSelect={handleSelect} />,
   ];
 
   let content = pages[pagePositon];
---
+++ FE/Noviny/src/pages/ArticlePage.jsx
@@ -0,0 +1,34 @@
+const data = [
+    {
+        image: "src/assets/main-page-1.jpg",
+        name: "Lorem Ipsum 1",
+        userName: "Jožo Ráž",
+        dateOfUpload: "19.4.2024",
+        text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        reviews: [
+            {
+              text: "Článok je dobrý",
+              reviewerName: "Peter Stastny",
+              id: "1234",
+            },
+            {
+                text: "Prečo toto je tak otrasné",
+                reviewerName: "Peter Stastny",
+                id: "1234",
+              },
+              {
+                text: "Neklam",
+                reviewerName: "Peter Stastny",
+                id: "1234",
+              },
+          ],
+    }
+]
+
+const ArticlePage = () => {
+    return (
+        <p>Copak</p>
+    )
+}
+
+export default ArticlePage;
