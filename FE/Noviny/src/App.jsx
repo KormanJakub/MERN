2bb950fe0aa450ec09274bf4289dd3a8aa2741fd
Jakub Korman <jakub.korman@siemens-healthineers.com>
2024-05-03 10:38:58 +0200

    FE: commit

--- FE/Noviny/src/App.jsx
+++ FE/Noviny/src/App.jsx
@@ -9,65 +9,56 @@ import ArticlePage from "./pages/ArticlePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import AddArticlePage from "./pages/AddArticlePage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import { checkAuthLoader, tokenLoader } from "./util/auth";
+import { action as loginAction } from "./util/loaders";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    loader: tokenLoader,
+    id: "root",
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "articles",
+        element: <ArticleBrowserPage />,
+      },
+      {
+        path: "add-article",
+        element: <AddArticlePage />,
+      },
+      {
+        path: "articles/:art_id",
+        element: <ArticlePage />,
+      },
+      {
+        path: "admin",
+        element: <AdminPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+      {
+        login: "login",
+        element: <LoginPage />,
+      },
+    ]
+  }
+]);
 
 function App() {
-  const [pagePositon, setPagePosition] = useState(0);
-  const [selectedArticle, setSelectedArticle] = useState(1);
-
-  const handleSelect = useCallback((selectedValue, art_id) => {
-    if (selectedValue === "home") {
-      setPagePosition(0);
-    }
-
-    if (selectedValue === "about") {
-      setPagePosition(1);
-    }
-
-    if (selectedValue === "articles") {
-      setPagePosition(2);
-    }
-
-    if (selectedValue === "articleDetail") {
-      setPagePosition(3);
-      setSelectedArticle(art_id);
-    }
-
-    if (selectedValue === "admin") {
-      setPagePosition(4);
-    }
-
-    if (selectedValue === "login") {
-      setPagePosition(5);
-    }
-
-    if (selectedValue === "register") {
-      setPagePosition(6);
-    }
-
-    if (selectedValue === "add-article") {
-      setPagePosition(7);
-    }
-  });
-
-  const pages = [
-    <HomePage key={0} onSelect={handleSelect} />,
-    <AboutPage key={1} />,
-    <ArticleBrowserPage key={2} onSelect={handleSelect} />,
-    <ArticlePage key={3} selectedArticle={selectedArticle} />,
-    <AdminPage key={4} onSelect={handleSelect} />,
-    <LoginPage key={5} />,
-    <RegisterPage key={6} />,
-    <AddArticlePage key={7} />,
-  ];
-
-  let content = pages[pagePositon];
-
-  return (
-    <Fragment>
-      <NavBar onSelect={handleSelect} />
-      {content}
-    </Fragment>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- FE/Noviny/src/components/NavBar.jsx
+++ FE/Noviny/src/components/NavBar.jsx
@@ -1,19 +1,31 @@
+import { Link, useNavigate, useRouteLoaderData } from "react-router-dom";
+
 const NavBar = ({ onSelect }) => {
+
+  const token = useRouteLoaderData("root");
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/");
+  };
 
   return (
     <nav>
       <div className="flex gap-5 justify-content-between bg-gray-400 pr-4 pl-4">
-      <div className="flex gap-5">
-        <p className="cursor-pointer" onClick={() => onSelect("home")}>NOVINY.SK</p>
-        <p className="cursor-pointer" onClick={() => onSelect("about")}>About</p>
-        <p className="cursor-pointer" onClick={() => onSelect("articles")}>Articles</p>
-      </div>
-      <div className="flex justify gap-5 justify-content-center">
-        <p className="cursor-pointer" onClick={() => onSelect("admin")}>Admin</p>
-        <p className="cursor-pointer" onClick={() => onSelect("register")}>User</p>
-        <p className="cursor-pointer" onClick={() => onSelect("login")}>Login</p>
-        <p className="cursor-pointer" onClick={() => onSelect("register")}>Register</p>
-      </div>
+        <div className="flex gap-5">
+          <p className="cursor-pointer" onClick={navigateHandler}>NOVINY.SK</p>
+          <p className="cursor-pointer" navTo="/about">
+            <Link to="/about">About</Link></p>
+          <p className="cursor-pointer">
+            <Link to="/articles">Articles</Link>
+          </p>
+        </div>
+        <div className="flex justify gap-5 justify-content-center">
+          <p className="cursor-pointer">
+            <Link to="/admin">Admin</Link></p>
+          <p className="cursor-pointer">User</p>
+          <p className="cursor-pointer"><Link to="/login">Login</Link></p>
+          <p className="cursor-pointer"><Link to="/register">Register</Link></p>
+        </div>
       </div>
     </nav>
   );
